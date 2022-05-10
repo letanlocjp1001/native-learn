7e11fb3212559733b44ae6024889ca59cc336c94
Kaneki <kaneki@MacBookPro-Kaneki.local>
2022-05-10 09:33:47 +0900

    learn

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  SafeAreaView,
+} from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
+import StartGameScreen from './screens/StartGameScreen'
+import GameScreen from './screens/GameScreen'
+import Colors from './constants/colors'
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+
+  const fickedNumberHandler = (pickedNumber) => {
+    setUserNumber(pickedNumber)
+  }
+
+  let screen = <StartGameScreen onPickNumber={fickedNumberHandler} />
+
+  if (userNumber) {
+    screen = <GameScreen />
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <LinearGradient
+      style={styles.rootScreen}
+      colors={[Colors.primary700, Colors.accent500]}
+    >
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        style={styles.rootScreen}
+        resizeMode='cover'
+        imageStyle={styles.backgroundImage}
+      >
+        <SafeAreaView style={styles.rootScreen}>{screen}</SafeAreaView>
+      </ImageBackground>
+    </LinearGradient>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  backgroundImage: {
+    opacity: 0.15,
+  },
+})
